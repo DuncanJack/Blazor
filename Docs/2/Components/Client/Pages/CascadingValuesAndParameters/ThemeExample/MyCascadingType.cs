e8421ef6a42c615d08876c87bed5b12b1cac0f60
Duncan Jack <duncan@openword.co.uk>
2020-01-17 15:04:30 +0930

    Theme example

---
+++ Docs/2/Components/Client/Pages/CascadingValuesAndParameters/ThemeExample/MyCascadingType.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Components.Client.Pages.CascadingValuesAndParameters.ThemeExample
+{
+    public class MyCascadingType
+    {
+        public string Name { get; set; }
+    }
+}
