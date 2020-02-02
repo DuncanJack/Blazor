6a9adb147bd57d7b697315f4cc66db8ed9ee9e0c
Duncan Jack <duncan@openword.co.uk>
2020-02-03 06:53:33 +0930

    Starship, EditContext

---
+++ Docs/2/FormsAndValidation/Pages/EditContext/MyModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsAndValidation.Pages.EditContext
+{
+    public class MyModel
+    {
+        [Required]
+        public string Identifier { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long (maximum 10 characters)")]
+        public string Name { get; set; }
+    }
+}
