0123e6f5086892001dc4f1fe07e2bae4d5bfcc8f
Duncan Jack <duncan@openword.co.uk>
2020-02-11 07:12:52 +0930

    Start Nested models

---
+++ Docs/2/FormsAndValidation/Pages/ValidationSupport/NestedModels/Starship.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsAndValidation.Pages.ValidationSupport.NestedModels
+{
+    public class Starship
+    {
+        [ValidateComplexType]
+        public ShipDescription ShipDescription { get; set; }
+    }
+}
